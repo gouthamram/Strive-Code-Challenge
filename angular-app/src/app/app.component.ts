a3f1fff4bb5c7e7f4f0453096de1d252cf63f361
Goutham Ramachandran <gouthamramachandran@Gouthams-MacBook-Pro.local>
2021-07-07 19:42:33 -0500

    checkpoint

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PEOPLE } from '../data/mock-data';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  title = 'The Person Finder';
+  public peopleList: any[] = PEOPLE;
+  public shortList: any[] = [];
+  
+  ngOnInit() {
+    this.shortList = _.take(this.peopleList, 3);
+    console.log(_.get(this.peopleList[0], 'name'));
+  }
 }
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatListModule } from '@angular/material/list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+
+
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ListContentComponent } from './list-content/list-content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ListContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
